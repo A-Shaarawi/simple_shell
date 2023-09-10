ff2e05fce339d689febbdbd71856320716e64d3b
A-Shaarawi <abdelrahmangamal305@gmail.com>
2023-09-10 06:57:25 -0700

    shell

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#endif
---
+++ simple-shell.c
@@ -0,0 +1,41 @@
+#include "shell.h"
+int main(void)
+{
+char *buff = NULL, *p = "$ ";
+size_t buffsize = 0;
+ssize_t bytes;
+pid_t wpid;
+int wstatus;
+
+bool from_pipe = false;
+while (1 && !from_pipe)
+{
+if (isatty(STDIN_FILENO) == 0)
+from_pipe = true;
+write(STDOUT_FILENO, p, 2);
+bytes = getline(&buff, &buffsize, stdin);
+if (bytes == -1)
+{
+perror("Error (getline)");
+free(buff);
+exit(EXIT_FAILURE);
+}
+if (buff[bytes - 1] == '\n')
+buff[bytes - 1] = '\0';
+wpid = fork();
+if (wpid == -1)
+{
+perror("Error (fork)");
+exit(EXIT_FAILURE);
+}
+if (wpid == 0)
+execvp(buff, &buff);
+if (waitpid(wpid, &wstatus, 0) == -1)
+{
+perror("Error (wait)");
+exit(EXIT_FAILURE);
+}
+}
+free(buff);
+return (0);
+}
