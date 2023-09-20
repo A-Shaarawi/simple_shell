e88229a4eaaddc1284effe6e94d73c8d286477db
A-Shaarawi <abdelrahmangamal305@gmail.com>
2023-09-20 05:59:10 -0700

    test

--- simple-shell.c
+++ simple-shell.c
@@ -1,65 +1,49 @@
 #include "shell.h"
-#define BUFFER_SIZE 1024
-#define MAX_ARGS 10
-
+/**
+ * main - fucntion
+ * Return: 0
+ */
 int main(void)
 {
-    char *buff = NULL;
-    size_t buffsize = 0;
-    ssize_t bytes;
-    pid_t wpid;
-    int wstatus;
-    char prompt[] = "$ ";
-    bool piping = false;
-
-    while (1 && !piping)
-    {
-        piping = (isatty(STDIN_FILENO) == 0);
-        write(STDOUT_FILENO, prompt, strlen(prompt));
-        bytes = getline(&buff, &buffsize, stdin);
-
-        if (bytes == -1)
-        {
-            perror("Error (getline)");
-            free(buff);
-            exit(EXIT_FAILURE);
-        }
-
-        if (buff[bytes - 1] == '\n')
-            buff[bytes - 1] = '\0';
-
-        wpid = fork();
-        if (wpid == -1)
-        {
-            perror("Error (fork)");
-            exit(EXIT_FAILURE);
-        }
-        else if (wpid == 0)
-        {
-            char *args[MAX_ARGS];
-            char *token;
-            int argIndex = 0;
-
-            token = strtok(buff, " ");
-            while (token != NULL && argIndex < MAX_ARGS - 1)
-            {
-                args[argIndex] = token;
-                argIndex++;
-                token = strtok(NULL, " ");
-            }
-            args[argIndex] = NULL;
-
-            execvp(args[0], args);
-            perror("Error ");
-            exit(EXIT_FAILURE);
-        }
-        else if (waitpid(wpid, &wstatus, 0) == -1)
-        {
-            perror("Error (wait)");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    free(buff);
-    return 0;
+char *buff = NULL, *args[2];
+size_t buffsize = 0;
+ssize_t bytes;
+pid_t wpid;
+int wstatus;
+char prompt[] = "$ ";
+bool piping = false;
+while (1 && !piping)
+{
+	piping = (isatty(STDIN_FILENO) == 0);
+write(STDOUT_FILENO, prompt, strlen(prompt));
+bytes = getline(&buff, &buffsize, stdin);
+if (bytes == -1)
+{
+perror("Error (getline)");
+free(buff);
+exit(EXIT_FAILURE);
+}
+if (buff[bytes - 1] == '\n')
+buff[bytes - 1] = '\0';
+wpid = fork();
+if (wpid == -1)
+{
+perror("Error ");
+exit(EXIT_FAILURE);
+}
+if (wpid == 0)
+{
+args[0] = buff, args[1] = NULL;
+execvp(args[0], args);
+perror("Error ");
+exit(EXIT_FAILURE);
+}
+if (waitpid(wpid, &wstatus, 0) == -1)
+{
+perror("Error (wait)");
+exit(EXIT_FAILURE);
+}
+}
+free(buff);
+return (0);
 }
