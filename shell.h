776302e1581d2fba4aa5963865681bd2391451ea
A-Shaarawi <abdelrahmangamal305@gmail.com>
2023-09-20 10:43:22 -0700

    SHELL

--- shell.h
+++ shell.h
@@ -11,8 +11,22 @@
 #include <sys/wait.h>
 #include <string.h>
 
-void display_prompt(void);
-void user_input(char *cmd, size_t size);
-void execute_command(const char *cmd);
+/**
+ * _strlen - funtion
+ * @s: parameter
+ * Return: number
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
 
 #endif
